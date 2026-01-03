da2aa455092d01febe659acfff1e517afeac33ee
Sek <t2transformer@yahoo.com>
2026-01-03 22:03:02 +0300

    init project

---
+++ src/main/java/ua/univer/pftsTest/TestPftsApplication.java
@@ -0,0 +1,13 @@
+package ua.univer.pftsTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestPftsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestPftsApplication.class, args);
+	}
+
+}
